8150a4bafbc5c51a3e553d8661048630030005b0
Jag Shelkay <50410385+orionJag@users.noreply.github.com>
2021-02-05 19:11:35 -0500

    Update PlotResults.py

--- MultiCamObjNV/PlotResults.py
+++ MultiCamObjNV/PlotResults.py
@@ -30,54 +30,63 @@ def plot_ObjDetectCount():
             ItemCount.append(row[1])
 
     sqlCursor.close()
+    plt.figure(figsize=(10,6)) 
     plt.bar(ItemDesc, ItemCount, color='red')
     plt.title('Objects Detected Count')
-    plt.xlabel('Item Desc')
-    plt.ylabel('Item Count')
+    plt.xlabel('Item Desc',  fontsize=10)
+    plt.ylabel('Item Count', fontsize=10)
     plt.savefig('Plots/Items_Detected1.jpeg')
     plt.close()
     #plt.show() 
 
     # Plot the distribution curve
+    plt.figure(figsize=(10,6)) 
     plt.pie(ItemCount, labels=ItemDesc, shadow=True, autopct='%1.1f%%')
-    plt.axis('equal')
-    #plt.legend(title="Object Detections")
+    plt.legend(title="Object Detections")
     plt.grid(True)
     plt.savefig('Plots/Items_Detected2.jpeg')
     plt.close()
+    ItemDesc.clear()
+    ItemCount.clear()
+
 
 # Plot details 
 def plot_ObjDetectCount_Time():
-    sqlStmt =  'SELECT count(distinct Item_Desc) as RecCnt, CrtTS FROM OpnCVStats Group by CrtTS'
+    sqlStmt =  'SELECT count(distinct Item_Desc) as RecCnt, CrtTS, substr(CrtTS, 15,5) as mmSec FROM OpnCVStats Group by CrtTS'
     sqlCursor = conn.cursor()
     sqlCursor.execute(sqlStmt)
     item_data = sqlCursor.fetchall()
 
     ItemCount =[]
     ItemCrtTS =[]
+    ItemMMSEC =[]
     for row in item_data:
             ItemCount.append(row[0])
             ItemCrtTS.append(row[1])
+            ItemMMSEC.append(row[2])
 
     sqlCursor.close()
-    plt.figure()
-    plt.plot(ItemCrtTS, ItemCount)  
-    plt.xlabel("TimeStamp")
-    plt.ylabel("ItemCount")
+    plt.figure(figsize=(10,6))   
+    plt.plot(ItemMMSEC, ItemCount)
+    plt.xlabel("TimeStamp", fontsize=8)
+    plt.ylabel("ItemCount", fontsize=8)
     plt.title("Object Detected over Multiple Cycles")
-    #plt.legend()
     plt.grid(True)
     plt.savefig('Plots/Items_Detected3.jpeg')
-    plt.close()
+    plt.close()    
+    ItemCount.clear()
+    ItemCrtTS.clear()
+    ItemMMSEC.clear()
 
 
     # Jetson Statistics (Temp)
-    sqlStmt =  'select CrtTS, jtempao, jtempcpu, jtempgpu, jtemppll, jtempthermal, jpowercur from OpnCVStats order by CrtTS' 
+    sqlStmt =  'select CrtTS, substr(CrtTS, 15,5) as mmSec, jtempao, jtempcpu, jtempgpu, jtemppll, jtempthermal, jpowercur from OpnCVStats order by CrtTS' 
     sqlCursor = conn.cursor()
     sqlCursor.execute(sqlStmt)
     item_data = sqlCursor.fetchall()
 
     ItemCrtTS =[]
+    ItemMMSEC=[]
     ItemTempao=[]
     ItemCpu=[]
     ItemGpu=[]
@@ -86,37 +95,42 @@ def plot_ObjDetectCount_Time():
     ItemPowercur=[]
     for row in item_data:
             ItemCrtTS.append(row[0])
-            ItemTempao.append(row[1])
-            ItemCpu.append(row[2])
-            ItemGpu.append(row[3])
-            ItemPll.append(row[4])
-            ItemThermal.append(row[5])
-            ItemPowercur.append(row[6])
+            ItemMMSEC.append(row[1])
+            ItemTempao.append(row[2])
+            ItemCpu.append(row[3])
+            ItemGpu.append(row[4])
+            ItemPll.append(row[5])
+            ItemThermal.append(row[6])
+            ItemPowercur.append(row[7])
 
     sqlCursor.close()
-    plt.figure()
-    plt.plot(ItemCrtTS, ItemTempao, label = "Temp AO")  
-    plt.plot(ItemCrtTS, ItemCpu, label = "Temp CPU")  
-    plt.plot(ItemCrtTS, ItemGpu, label = "Temp GPU")  
-    plt.plot(ItemCrtTS, ItemPll, label = "Temp PLL")  
-    plt.plot(ItemCrtTS, ItemThermal, label = "Temp Thermal")  
-    plt.plot(ItemCrtTS, ItemPowercur, label = "Temp PowerCur")  
-    plt.xlabel("TimeStamp")
-    plt.ylabel("Jetson Temperature")
+    plt.figure(figsize=(10,6)) 
+    plt.plot(ItemMMSEC, ItemTempao, label = "Temp AO")  
+    plt.plot(ItemMMSEC, ItemCpu, label = "Temp CPU")  
+    plt.plot(ItemMMSEC, ItemGpu, label = "Temp GPU")  
+    plt.plot(ItemMMSEC, ItemPll, label = "Temp PLL")  
+    plt.plot(ItemMMSEC, ItemThermal, label = "Temp Thermal")  
+    plt.plot(ItemMMSEC, ItemPowercur, label = "Temp PowerCur")  
+    plt.xlabel("TimeStamp", fontsize=8)
+    plt.ylabel("Jetson Temperature", fontsize=8)
     plt.title("Jetson Temp over  Multiple Cycles")
     plt.legend()
     plt.grid(True)
     plt.savefig('Plots/JetsonTemp.jpeg')
-    #plt.show()
     plt.close()
+    #plt.show()
+    ItemCrtTS.clear()
+    ItemMMSEC.clear()
+
 
     # Jetson Statistics (CPU, GPU)
-    sqlStmt =  'select CrtTS, jcpu1, jcpu2, jcpu3, jcpu4, jgpu from OpnCVStats order by CrtTS' 
+    sqlStmt =  'select CrtTS, substr(CrtTS, 15,5) as mmSec, jcpu1, jcpu2, jcpu3, jcpu4, jgpu from OpnCVStats order by CrtTS' 
     sqlCursor = conn.cursor()
     sqlCursor.execute(sqlStmt)
     item_data = sqlCursor.fetchall()
 
     ItemCrtTS =[]
+    ItemMMSEC=[]
     ItemCpu1=[]
     ItemCpu2=[]
     ItemCpu3=[]
@@ -124,28 +138,28 @@ def plot_ObjDetectCount_Time():
     ItemGpu1=[]
     for row in item_data:
             ItemCrtTS.append(row[0])
-            ItemCpu1.append(row[1])
-            ItemCpu2.append(row[2])
-            ItemCpu3.append(row[3])
-            ItemCpu4.append(row[4])
-            ItemGpu1.append(row[5])
-
+            ItemMMSEC.append(row[1])
+            ItemCpu1.append(row[2])
+            ItemCpu2.append(row[3])
+            ItemCpu3.append(row[4])
+            ItemCpu4.append(row[5])
+            ItemGpu1.append(row[6])
 
     sqlCursor.close()
-    plt.figure()
-    plt.plot(ItemCrtTS, ItemCpu1, label = "CPU1")  
-    plt.plot(ItemCrtTS, ItemCpu2, label = "CPU2")  
-    plt.plot(ItemCrtTS, ItemCpu3, label = "CPU3")  
-    plt.plot(ItemCrtTS, ItemCpu4, label = "CPU4")  
-    plt.plot(ItemCrtTS, ItemGpu1, label = "GPU1")  
-    plt.xlabel("TimeStamp")
-    plt.ylabel("Jetson Procs")
+    plt.figure(figsize=(10,6)) 
+    plt.plot(ItemMMSEC, ItemCpu1, label = "CPU1")  
+    plt.plot(ItemMMSEC, ItemCpu2, label = "CPU2")  
+    plt.plot(ItemMMSEC, ItemCpu3, label = "CPU3")  
+    plt.plot(ItemMMSEC, ItemCpu4, label = "CPU4")  
+    plt.plot(ItemMMSEC, ItemGpu1, label = "GPU1")  
+    plt.xlabel("TimeStamp", fontsize=8)
+    plt.ylabel("Jetson Procs", fontsize=8)
     plt.title("Jetson Procs Stats over Multiple Cycles")
-    plt.axis()
-    plt.legend()
     plt.grid(True)
     plt.savefig('Plots/JetsonProcs.jpeg') 
     plt.close()
+    ItemCrtTS.clear()
+    ItemMMSEC.clear()
 
 
 # # plot the graphs 
